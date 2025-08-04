febfb431d089353977fff63b784fcada121506a7
Zoltan Karahuta <zoltan.karahuta@indisemi.com>
2025-08-04 23:46:12 +0200

    Added slrts

--- bubbleshort/src/main.rs
+++ bubbleshort/src/main.rs
@@ -1,10 +1,10 @@
 use rand::Rng;
 
 fn main() {
-    let mut vector = gen_rand_vec(-1000, 1000, 1000);
+    let mut vector = gen_rand_vec(-1000, 1000, 20);
     println!("This is the original vector: ");
     println!("{:?}",vector);
-    bubble_sort(&mut vector);
+    shaker_sort(&mut vector);
     println!("This the vecor after sorting");
     println!("{:?}",vector);
 }
@@ -20,8 +20,7 @@ fn get_rand(min: i32, max: i32) -> i32 {
 }
 fn bubble_sort(data: &mut Vec<i32> ) -> &mut Vec<i32> {
     let size = data.len();
-    if size < 1 { return data };
-
+    if size < 2 { return data };
     let mut count = 0;
     while count < size {
         for i in 1..(size-count) {
@@ -30,6 +29,37 @@ fn bubble_sort(data: &mut Vec<i32> ) -> &mut Vec<i32> {
             }
         }
         count += 1;
+        println!("Iteration: {:?}",data);
     }
     data
 }
+fn shaker_sort(data: &mut Vec<i32> ) -> &mut Vec<i32> {
+    let size = data.len();
+    if size < 2 { return data };
+
+    let mut count = 0;
+    let mut sorted = false;
+    while count < size {
+        sorted = true;
+        for i in (count+1..(size-count)) {
+            if *(data.get(i).unwrap()) <  *(data.get(i-1).unwrap()) {
+                data.swap(i, i-1);
+                sorted = false;
+            }
+        }
+        println!("Up-shake:   {:?}",data);
+        for i in (count..(size-count-1)).rev() {
+            //println!("{i}");
+            if *(data.get(i).unwrap()) >  *(data.get(i+1).unwrap()) {
+                data.swap(i, i+1);
+                sorted = false;
+            }
+        }
+        println!("Down-shake: {:?}",data);
+        count += 1;
+        if sorted { return data; }
+    }
+    data
+}
+
+
